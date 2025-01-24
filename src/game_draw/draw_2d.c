69a347adc7f6e88307cb77b958afed8e0a42c72e
Florent Tapponnier <ftapponn@2-H-9.42heilbronn.de>
2025-01-24 19:16:48 +0100

    clean

--- src/game_draw/draw_2d.c
+++ src/game_draw/draw_2d.c
@@ -1,21 +1,34 @@
 #include "../../includes/cub3d.h"
 
-void draw_map(t_game *game)
+void	draw_map(t_game *game)
 {
-    char **map = game->map;
-    for(int y = 0; map[y]; y++)
-        for(int x = 0; map[y][x]; x++)
-            if(map[y][x] == '1')
-                draw_square(x * BLOCK, y * BLOCK, BLOCK, SQUARE2D_COLOR, game);
+	char	**map;
+	int		y;
+	int		x;
+
+	map = game->map;
+	y = 0;
+	while (map[y])
+	{
+		x = 0;
+		while (map[y][x])
+		{
+			if (map[y][x] == '1')
+				draw_square(x * BLOCK, y * BLOCK, BLOCK, SQUARE2D_COLOR, game);
+			x++;
+		}
+		y++;
+	}
 }
 
-void draw_square(int x, int y, int size, int color, t_game *game)
+void	draw_square(int x, int y, int size, int color, t_game *game)
 {
-	int i = -1;
-	
+	int	i;
+
+	i = -1;
 	while (++i < size)
 		mlx_put_pixel(game->img, x + i, y, color);
-	i = - 1;
+	i = -1;
 	while (++i < size)
 		mlx_put_pixel(game->img, x, y + i, color);
 	i = -1;
@@ -26,7 +39,8 @@ void draw_square(int x, int y, int size, int color, t_game *game)
 		mlx_put_pixel(game->img, x + i, y + size, color);
 }
 
-void draw_raycast_line(mlx_image_t *image, uint32_t ray_x, uint32_t ray_y, uint32_t color)
+void	draw_raycast_line(mlx_image_t *image, uint32_t ray_x, uint32_t ray_y,
+		uint32_t color)
 {
-    mlx_put_pixel(image, ray_x, ray_y, color);
+	mlx_put_pixel(image, ray_x, ray_y, color);
 }
