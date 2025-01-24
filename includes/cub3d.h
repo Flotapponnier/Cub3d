f7e6fcb94199cf8a914c67ad9a30454a1131ce8f
Florent Tapponnier <ftapponn@3-I-5.42heilbronn.de>
2025-01-24 12:37:46 +0100

    corrected files mlx

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -11,14 +11,8 @@
 #define WIDTH 1280
 #define HEIGHT 720
 
-#define W 119
-#define A 97
-#define S 115
-#define D 100
 #define PI 3.14159265359
 #define BLOCK 64
-#define LEFT 65361
-#define RIGHT 65363
 #define DEBUG 0
 
 #define SIZE_PLAYER 10
