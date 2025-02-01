db3232e64f040066b6aff1e01291047406d724cb
Florent Tapponnier <ftapponn@2-I-1.42heilbronn.de>
2025-02-01 11:31:10 +0100

    remove distance function (useless)

--- src/raycaster/raycasting.c
+++ src/raycaster/raycasting.c
@@ -6,30 +6,12 @@
 /*   By: ftapponn <ftapponn@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 13:32:09 by ftapponn          #+#    #+#             */
-/*   Updated: 2025/02/01 11:28:05 by ftapponn         ###   ########.fr       */
+/*   Updated: 2025/02/01 11:30:27 by ftapponn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
-static float distance(float x, float y)
-{
-	return sqrt(x * x + y * y);
-}
-
-float fixed_distance(float x1, float y1, float x2, float y2, t_game *game)
-{
-	float delta_x;
-	float delta_y;
-	float angle;
-	float fixed_distance;
-
-	delta_x = x2 - x1;
-	delta_y = y2 - y1;
-	angle = atan2(delta_y, delta_x) - game->player.angle;
-	fixed_distance = distance(delta_x, delta_y) * cos(angle);
-	return (fixed_distance);
-}
 
 void	init_ray(t_player *player, t_ray *ray, int x)
 {
