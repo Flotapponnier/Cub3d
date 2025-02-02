e7597217ee4e1218886f087b753c5b387981d1a6
ilindaniel <25451549+ilindaniel@users.noreply.github.com>
2025-02-02 20:28:13 +0100

    Check for leaks

--- src/main.c
+++ src/main.c
@@ -6,16 +6,22 @@
 /*   By: ftapponn <ftapponn@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 13:31:55 by ftapponn          #+#    #+#             */
-/*   Updated: 2025/02/01 19:51:53 by dilin            ###   ########.fr       */
+/*   Updated: 2025/02/02 20:26:43 by dilin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
+void	leaks(void)
+{
+	system("leaks cub3D");
+}
+
 int	main(int argc, char **argv)
 {
 	t_game	game;
 
+	atexit(leaks);
 	if (argc != 2)
 		return (error(ERROR_ARG), 1);
 	init_game(&game, argv[1]);
